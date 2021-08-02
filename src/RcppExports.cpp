df5614508ee23f652294561676b51f6070a85c0c
Carlos Biagi Jr <biagi@usp.br>
2021-08-02 15:16:59 -0300

    Update src folder

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // tolerance
 float tolerance(float a, float b, float c, int tolType);
 RcppExport SEXP _CeTF_tolerance(SEXP aSEXP, SEXP bSEXP, SEXP cSEXP, SEXP tolTypeSEXP) {
