6300a54b95ee719e4e357237a34bb593d9a06885
Carlos Biagi Jr <biagi@usp.br>
2021-08-10 16:30:22 -0300

    Update

--- R/enrichPlot.R
+++ R/enrichPlot.R
@@ -14,7 +14,6 @@
 #' @importFrom ggplot2 ggplot aes geom_bar coord_polar theme_minimal theme 
 #' element_blank labs coord_flip theme_bw scale_x_discrete scale_fill_gradient 
 #' geom_point scale_color_gradient scale_size_continuous scale_fill_continuous
-#' @importFrom utils read.table
 #'
 #' @examples
 #' # loading enrichdemo
@@ -45,34 +44,33 @@ enrichPlot <- function(res, showCategory = 10, type = "circle") {
     }
     
     res <- head(res, showCategory)
-    res$enrichmentRatio <- with(read.table(text = res[['GeneRatio']], sep = "/"), V1/V2)
     
     if (type == "circle") {
-        pt <- ggplot(res, aes(x = .data[["Description"]], y = .data[["enrichmentRatio"]]), 
+        pt <- ggplot(res, aes(x = .data[["description"]], y = .data[["enrichmentRatio"]]), 
             colour = .data[["enrichmentRatio"]]) + geom_bar(aes(fill = .data[["enrichmentRatio"]], 
-            alpha = .data[["p.adjust"]]), stat = "identity", position = "stack") + 
+            alpha = .data[["FDR"]]), stat = "identity", position = "stack") + 
             coord_polar(clip = "off") + theme_minimal() + theme(axis.title.x = element_blank(), 
             axis.title.y = element_blank(), axis.text.y = element_blank(), 
             axis.text.x = element_text(color = "grey20", size = 8, hjust = 0.5, 
                 vjust = 0.5, face = "plain"), legend.position = "bottom") + 
-            labs(fill = "enrichmentRatio", alpha = "p.adjust")
+            labs(fill = "enrichmentRatio", alpha = "FDR")
     } else if (type == "bar") {
         positions <- res[order(res[["enrichmentRatio"]], decreasing = FALSE), 
-            "Description"]
-        pt <- ggplot(data = res, aes(x = .data[["Description"]], y = .data[["enrichmentRatio"]])) + 
-            geom_bar(stat = "identity", aes(fill = .data[["p.adjust"]])) + coord_flip() + 
+            "description"]
+        pt <- ggplot(data = res, aes(x = .data[["description"]], y = .data[["enrichmentRatio"]])) + 
+            geom_bar(stat = "identity", aes(fill = .data[["FDR"]])) + coord_flip() + 
             theme_bw() + scale_x_discrete(limits = positions) + scale_fill_gradient(low = "#F4B41A", 
             high = "#143D59", trans = "reverse") + theme(axis.title.x = element_blank(), 
-            axis.title.y = element_blank()) + labs(fill = "p.adjust")
+            axis.title.y = element_blank()) + labs(fill = "FDR")
     } else if (type == "dot") {
         positions <- res[order(res[["enrichmentRatio"]], decreasing = FALSE), 
-            "Description"]
-        pt <- ggplot(res, aes(x = .data[["Description"]], y = .data[["enrichmentRatio"]], 
-            size = .data[["Count"]], color = .data[["p.adjust"]])) + geom_point(alpha = 0.8) + 
+            "description"]
+        pt <- ggplot(res, aes(x = .data[["description"]], y = .data[["enrichmentRatio"]], 
+            size = .data[["overlap"]], color = .data[["FDR"]])) + geom_point(alpha = 0.8) + 
             theme_bw() + scale_color_gradient(low = "#F4B41A", high = "#143D59", 
             trans = "reverse") + scale_x_discrete(limits = positions) + 
             coord_flip() + theme(axis.title.x = element_blank(), axis.title.y = element_blank()) + 
-            labs(color = "p.adjust", size = "Count") + scale_size_continuous(range = range(res[["Count"]]))
+            labs(color = "FDR", size = "Count") + scale_size_continuous(range = range(res[["overlap"]]))
     }
     return(pt)
 }
